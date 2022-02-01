1aed1878dae6806e32c777433e87d096f1a5f379
Hannah Glazier <hrayglazier@aol.com>
2022-02-01 11:17:43 -0700

    commit tree d455f381d3fd9ce6f6a6bef8a4045e169de9b999

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'fred';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'jim';
+}
